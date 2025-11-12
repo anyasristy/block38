2ac7aac7848feef8fb7346defed3a96629ef3891
anyasristy <148508255+anyasristy@users.noreply.github.com>
2025-11-12 18:23:20 -0500

    block 38

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import employeesRouter from "#api/employees.js";
 const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Fullstack Employees API.");
+});
+app.use("/employees", employeesRouter);
 export default app;
-
-// TODO: this file!
